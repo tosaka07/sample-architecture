256ede55540fd1a98bb4ee35ed31f15832f0e8d6
tosaka07 <tosaka.tech@gmail.com>
2019-01-12 14:19:56 +0900

    Initial Commit

---
+++ sample-architecture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sample-architecture
+//
+//  Created by 坂上 翔悟 on 2019/01/12.
+//  Copyright © 2019 tosaka.net. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
