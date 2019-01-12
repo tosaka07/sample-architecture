ba65e23a7e747b83d82b9f2e0a4757f408418d22
tosaka07 <tosaka.tech@gmail.com>
2019-01-12 15:26:55 +0900

    Add DataLayer

---
+++ SampleArchitectureDataLayer/SampleArchitectureDataLayer.h
@@ -0,0 +1,19 @@
+//
+//  SampleArchitectureDataLayer.h
+//  SampleArchitectureDataLayer
+//
+//  Created by 坂上 翔悟 on 2019/01/12.
+//  Copyright © 2019 tosaka.net. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SampleArchitectureDataLayer.
+FOUNDATION_EXPORT double SampleArchitectureDataLayerVersionNumber;
+
+//! Project version string for SampleArchitectureDataLayer.
+FOUNDATION_EXPORT const unsigned char SampleArchitectureDataLayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SampleArchitectureDataLayer/PublicHeader.h>
+
+
