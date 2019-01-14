be05b3e2a0b82479b4988ed83957b7a4d10ed161
tosaka07 <tosaka.tech@gmail.com>
2019-01-15 00:41:31 +0900

    Add DomainLayer framework.

---
+++ SampleArchitectureDomainLayer/SampleArchitectureDomainLayer.h
@@ -0,0 +1,19 @@
+//
+//  SampleArchitectureDomainLayer.h
+//  SampleArchitectureDomainLayer
+//
+//  Created by 坂上 翔悟 on 2019/01/15.
+//  Copyright © 2019 tosaka.net. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SampleArchitectureDomainLayer.
+FOUNDATION_EXPORT double SampleArchitectureDomainLayerVersionNumber;
+
+//! Project version string for SampleArchitectureDomainLayer.
+FOUNDATION_EXPORT const unsigned char SampleArchitectureDomainLayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SampleArchitectureDomainLayer/PublicHeader.h>
+
+
